6178ff56e5e6f7f2bb7738b3ecd01138feedd93d
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-10 23:49:44 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from 'tsdown'
 import Quansync from 'unplugin-quansync/rolldown'
 
 export default defineConfig({
-  entry: ['./src/{index,register,register-sync,worker}.ts'],
-  target: 'node18.19',
-  clean: true,
-  dts: true,
-  platform: 'node',
-  plugins: [
-    Quansync({
-      exclude: ['**/*.d.ts'],
-    }),
-  ],
+  entry: './src/{index,register,register-sync,worker}.ts',
+  plugins: [Quansync()],
 })
